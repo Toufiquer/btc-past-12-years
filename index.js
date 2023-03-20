9f7ef495c21bae6a6929489e995c04b10f6376e3
Toufiquer <toufiquerabc@gmail.com>
2023-03-20 07:54:31 +0600

    overview complete

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 import data from "./downloadData/json/btc2022.json" assert { type: "json" };
 
-const newArr = data.sort((a, b) => {
-  //   if (a["Date"] === "Nov 10, 2022") {
-  //     const aValue = parseFloat(a["Change %"].slice(0, -1));
-  //     console.log(a, aValue > 7, " => Line No: 6");
-  //   }
+const sortArr = (a, b, type) => {
   const aValue = parseFloat(a["Change %"].slice(0, -1));
   const bValue = parseFloat(b["Change %"].slice(0, -1));
-
-  if (aValue === bValue) {
-    return 0;
-  } else if (aValue < bValue) {
-    return 1;
+  if (type) {
+    if (aValue === bValue) {
+      return 0;
+    } else if (aValue < bValue) {
+      return 1;
+    } else {
+      return -1;
+    }
   } else {
-    return -1;
+    if (aValue === bValue) {
+      return 0;
+    } else if (aValue > bValue) {
+      return 1;
+    } else {
+      return -1;
+    }
   }
+};
+const positiveArr = [...data];
+const negativeArr = [...data];
+const newArrPositive = positiveArr.sort((a, b) => {
+  return sortArr(a, b, true);
 });
-newArr.length = 3;
-console.log(newArr, " => Line No: 5");
+const newArrNegative = negativeArr.sort((a, b) => {
+  return sortArr(a, b, false);
+});
+newArrPositive.length = 1;
+newArrNegative.length = 1;
+console.log(
+  "Max Positive ===>>>Date: " + newArrPositive[0].Date,
+  "Max Positive ===>>>Change: " + newArrPositive[0]["Change %"]
+);
+console.log(
+  "Max Negative ===>>>Date: " + newArrNegative[0].Date,
+  "Max Negative ===>>>Change: " + newArrNegative[0]["Change %"]
+);
